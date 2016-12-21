729d2e9dbf3ee256c743cbd472c0a02a31522830
Shankar Pandey <shankarp@in.ibm.com>
2016-12-21 08:02:16 -0600

    2

--- server.py
+++ server.py
@@ -12,19 +12,17 @@ except ImportError:
 PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 
-name = "bmix_dal_yp_894e58c3_c77e_4c0f_972c_bbc6d91f2733"
-#uri_cli = "mongo --ssl --sslAllowInvalidCertificates bluemix-sandbox-dal-9-portal.4.dblayer.com:20712/admin -u admin -p EJMSKZJPETYIWCCK"
-uri = "mongodb://admin:EJMSKZJPETYIWCCK@bluemix-sandbox-dal-9-portal.4.dblayer.com:20712,bluemix-sandbox-dal-9-portal.0.dblayer.com:20712/admin?ssl=true"
+uri = "mongodb://admin:VSBVBFCGCIFGXQFS@bluemix-sandbox-dal-9-portal.0.dblayer.com:19651/admin?ssl=true"
 
 client = MongoClient()
 
 client = MongoClient(uri)
 print client
 
-db = client.Prototypes
+db = client.LCHAnlyzer
 print db
 
-ReadData = db.Data_train_bpk
+ReadData = db.Test
 ReadData_bpk = ReadData.find()
 for doc in ReadData_bpk:
     print doc
